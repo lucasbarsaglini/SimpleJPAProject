d8882a03fdaa65b03ee34abfe64331262a889e88
lucasbarsaglini <lucasbarsag@gmail.com>
2022-07-09 11:12:10 -0300

    Insert data

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,17 +1,27 @@
 package application;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import domain.Person;
 
 public class Program {
 
 	public static void main(String[] args) {
 
-		Person p1 = new Person(1, "Carlos", "carlos@gmail.com");
-		Person p2 = new Person(2, "Joaquim", "joaquim@gmail.com");
-		Person p3 = new Person(3, "Ana", "ana@gmail.com");
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
+		Person p1 = new Person(null, "Carlos", "carlos@gmail.com");
+		Person p2 = new Person(null, "Joaquim", "joaquim@gmail.com");
+		Person p3 = new Person(null, "Ana", "ana@gmail.com");
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+		em.getTransaction().commit();
+		System.out.println("The end!");
 	}
 
 }
