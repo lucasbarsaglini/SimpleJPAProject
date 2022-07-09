2580a5ea2a8044e4c63fb14c6a1730bb72efbc49
lucasbarsaglini <lucasbarsag@gmail.com>
2022-07-09 11:25:22 -0300

    Data access

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -10,18 +10,15 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		Person p1 = new Person(null, "Carlos", "carlos@gmail.com");
-		Person p2 = new Person(null, "Joaquim", "joaquim@gmail.com");
-		Person p3 = new Person(null, "Ana", "ana@gmail.com");
-		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
 		EntityManager em = emf.createEntityManager();
-		em.getTransaction().begin();
-		em.persist(p1);
-		em.persist(p2);
-		em.persist(p3);
-		em.getTransaction().commit();
+		
+		Person p = em.find(Person.class, 2);
+		System.out.println(p);		
+		
 		System.out.println("The end!");
+		em.close();
+		emf.close();
 	}
 
 }
